94981213dfe689fb24c38589f5d8f221ec1826c0
AzStowe <aaron.z.stowe@gmail.com>
2019-06-24 11:07:58 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react"
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    label: "Party!!!"
+  }
+
+  handleChange = event => {
+    this.setState({
+      label: event.currentTarget.value
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>{this.state.label}</h1>
+        <input value={this.state.label} onChange={this.handleChange} />
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
